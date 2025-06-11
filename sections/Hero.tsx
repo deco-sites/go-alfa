cb095c62715ac61d806241c1b011470581201cc4
Danilo Santos <dev.danilosantos@gmail.com>
2025-06-11 20:22:14 -0300

    refactor: Ajusta título para permitir responsividade com Tailwind em vez de rich-text

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,4 +1,3 @@
-// deno-lint-ignore-file react-no-danger
 import type { ImageWidget } from "apps/admin/widgets.ts";
 
 export interface CTA {
@@ -9,14 +8,7 @@ export interface CTA {
 }
 
 export interface Props {
-  /**
-   * @format rich-text
-   * @default Click here to tweak this text however you want.
-   */
   title?: string;
-  /**
-   * @default This text is fully editable and ready for your personal touch. Just click here, head over to the section window, or dive straight into the code to make changes as you see fit. Whether it's about the content, formatting, font, or anything in between, editing is just a click away.
-   */
   description?: string;
   image?: ImageWidget;
   placement?: "center" | "bottom" | "top";
@@ -44,18 +36,14 @@ export default function HeroFlats({
           backgroundSize: `${size}`,
           backgroundPosition: `${placement}`,
         }}
-        class="flex flex-col items-center gap-8 h-full before:bg-black before:opacity-45 before:absolute before:inset-0 before:z-0"
+        class="flex flex-col items-center gap-8 h-full before:bg-black transition-all duration-300 before:opacity-65 before:md:opacity-45 before:absolute before:inset-0 before:z-0"
       >
         <div class="w-full lg:max-w-[1200px]  h-full  z-10 flex justify-center items-center ">
           <div class=" flex justify-center items-center flex-col gap-10 px-4">
-            <div
-              class="inline-block lg:text-[80px] text-center font-bold text-4xl  leading-none"
-              dangerouslySetInnerHTML={{
-                __html: title,
-              }}
-            >
+            <div class="inline-block text-4xl  text-center font-bold md:text-5xl text-primary  leading-none">
+              {title}
             </div>
-            <p class=" md:text-md leading-[150%] text-center text-white/80 text-2xl">
+            <p class=" md:text-md leading-[150%] text-center text-primary/80 text-2xl">
               {description}
             </p>
             <div class="flex justify-center items-center gap-3  h-10 w-full text-3xl">
@@ -66,7 +54,7 @@ export default function HeroFlats({
                   href={item?.href}
                   target={item?.href.includes("http") ? "_blank" : "_self"}
                   rel="noopener noreferrer"
-                  class={`font-bold btn btn-primary uppercase text-base    ${
+                  class={`font-bold btn px-10  btn-primary uppercase text-base    ${
                     item.outline && "btn-outline"
                   }`}
                 >
