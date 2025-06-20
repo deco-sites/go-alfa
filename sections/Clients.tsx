cd78a64bb1bf29955bc2f9f089bd7dddc1b681a9
Danilo Santos <dev.danilosantos@gmail.com>
2025-06-20 20:35:04 -0300

    feat: adiciona a seção Clientes ao projeto

---
+++ sections/Clients.tsx
@@ -0,0 +1,39 @@
+import TitleSection from "site/sections/TitleSection.tsx";
+import { ImageWidget } from "apps/admin/widgets.ts";
+import CustomCarousel from "site/islands/CustomCarousel.tsx";
+
+export interface Props {
+    title: string;
+    subtitle?: string;
+    description?: string;
+    images?: ImageWidget[];
+    width?: number;
+    height?: number;
+}
+
+const Clients = (
+    { title, subtitle, description, images, width, height }: Props,
+) => {
+    return (
+        <div
+            class={"lg:container md:max-w-7xl lg:mx-auto mx-0 md:mx-4 pb-12"}
+        >
+            <TitleSection
+                title={title}
+                subtitle={subtitle}
+                description={description}
+            />
+            <div
+                style={{
+                    maskImage:
+                        "linear-gradient(to right, transparent, black 20%, black 90%, transparent)",
+                }}
+                class={"lg:container md:max-w-7xl lg:mx-auto mx-0 md:mx-4 pb-12  "}
+            >
+                <CustomCarousel images={images} width={width} height={height} />
+            </div>
+        </div>
+    );
+};
+
+export default Clients;
