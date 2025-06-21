a3d2e4eb5acf5ff077b86048644d49caaaaf052f
Danilo Santos <dev.danilosantos@gmail.com>
2025-06-21 12:09:41 -0300

    style: modifica as cores do footer

--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div class={"text-center p-10"}>
+    <div class={"text-center p-10 bg-primary text-base-100 "}>
       Copyright © 2023 Allfa – Todos os direitos reservados.
     </div>
   );
